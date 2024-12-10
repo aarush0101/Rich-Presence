717d5f9c293910f61b0ab594fe2b9f556fcceb25
aarush0101 <riyansarma76@gmail.com>
2024-12-10 22:56:24 +0530

    chore: fix all issues

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const WebSocket = require("ws");
 require("dotenv").config();
+const WebSocket = require("ws");
+const Eris = require("eris");
 const keep_alive = require("./src/keep_alive.js");
+const {
+  logInfo,
+  logError,
+  validateEnvVariables,
+  isStartEnabled,
+  isActivityDisabled,
+  isTrue,
+  isFalse,
+  logDebug,
+} = require("./src/utilities.js");
 
-const GATEWAY_URL = "wss://gateway.discord.gg/?v=10&encoding=json";
+if (!isStartEnabled()) {
+  logError("[ERROR] No permission to start the server...");
+  process.exit(1);
+}
 
-let ws;
+const GATEWAY_URL = "wss://gateway.discord.gg/?v=9&encoding=json";
 let isActivityRunning = false;
-let heartbeatInterval; 
+let ws;
+let heartbeatInterval;
 let shouldReconnect = true;
+let eris = null;
 
 const bot = new Client({
   intents: [
@@ -17,15 +33,6 @@ const bot = new Client({
     GatewayIntentBits.Guilds,
   ],
 });
-
-bot.once('ready', () => {
-  bot.user.setPresence({
-    status: 'invisible', 
-  });
-  console.log('Bot is now invisible.');
-});
-
-
 
 function getVariables() {
   const variables = {
@@ -36,7 +43,7 @@ function getVariables() {
     smallImageUrl: process.env.SMALL_IMAGE_URL,
     smallText: process.env.SMALL_TEXT,
     largeText: process.env.LARGE_TEXT,
-    timestamps: process.env.TIMESTAMPS, 
+    timestamps: process.env.TIMESTAMPS,
     state: process.env.STATE,
     details: process.env.DETAILS,
     type: process.env.TYPE,
@@ -50,31 +57,9 @@ function getVariables() {
 
 const variables = getVariables();
 
-const BOT_TOKEN = variables.botToken;
-const USER_TOKEN = variables.userToken;
-const CHANNEL_ID = variables.channelId;
-
-if (!["true", "yes", "continue", "y"].includes(variables.start.toLowerCase())) {
-  console.error(
-    "Missing permission to start activity. Please re-check your configuration and try again."
-  );
-  process.exit(1);
-}
-
-if (!BOT_TOKEN || !USER_TOKEN || !CHANNEL_ID) {
-  console.error(
-    "Missing BOT_TOKEN or USER_TOKEN or CHANNEL_ID in .env file. Please re-check your configuration and try again."
-  );
-  process.exit(1);
-}
-
-function getJSON() {
-  if (
-    !["true", "yes", "continue", "y"].includes(variables.start.toLowerCase())
-  ) {
-    console.error(
-      "Missing permission to start activity. Please re-check your configuration and try again."
-    );
+function structureData() {
+  if (!isStartEnabled) {
+    logError("No permissions to start the server...");
     process.exit(1);
   }
 
@@ -93,11 +78,7 @@ function getJSON() {
     },
   ];
 
-  // Only add timestamps if noActivity is "false"
-  if (
-    !["false", "no", "back", "n"].includes(variables.timestamps.toLowerCase()) &&
-    !["true", "yes", "continue", "y"].includes(variables.noActivity.toLowerCase())
-  ) {
+  if (isTrue("TIMESTAMPS")) {
     activities[0].timestamps = { start: Date.now() };
   }
 
@@ -105,61 +86,44 @@ function getJSON() {
     since: Date.now(),
     status: variables.status,
     afk: false,
+    activities: activities,
   };
 
-  // Add activities only if noActivity is not "true", "no", "back", or "n"
-  if (!["true", "no", "back", "n"].includes(variables.noActivity.toLowerCase())) {
-    data.activities = activities;
-  }
-
-  // If noActivity is true, ensure activities are omitted
-  if (variables.noActivity.toLowerCase() === "true") {
-    delete data.activities; // Explicitly remove activities if noActivity is true
-  }
-
-  console.log("Payload being sent:", JSON.stringify({ op: 3, d: data }, null, 2));
   return data;
 }
 
 function setRichPresence() {
-  // Check WebSocket state before sending
   if (!ws || ws.readyState !== WebSocket.OPEN) return;
 
-  const data = getJSON();
+  const data = structureData();
 
   ws.send(
     JSON.stringify({
-      op: 3, 
+      op: 3,
       d: data,
     })
   );
 
-  console.log("Rich Presence successfully set.");
+  logDebug("Rich presence successfully sent...");
 }
 
 function connectUserGateway() {
-  if (ws) ws.close(); // Close any existing WebSocket connection
+  if (ws) ws.close();
 
   ws = new WebSocket(GATEWAY_URL);
 
   ws.on("open", () => {
-    console.log("Connected to Discord Gateway...");
+    logInfo("Connected to Discord Gateway...");
 
-    // Send Identify payload
     ws.send(
       JSON.stringify({
         op: 2,
         d: {
-          token: USER_TOKEN,
+          token: variables.userToken,
           properties: {
             os: "Windows",
             browser: "Chrome",
-            device: "desktop",
-            browser_user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-            browser_version: "106.0.0.0",
-            os_version: "10",
-            release_channel: "stable",
-            client_build_number: 134113,
+            device: null,
           },
         },
       })
@@ -174,101 +138,188 @@ function connectUserGateway() {
 
       if (heartbeatInterval) clearInterval(heartbeatInterval);
 
-      // Send heartbeats at the specified interval
       heartbeatInterval = setInterval(() => {
         ws.send(JSON.stringify({ op: 1, d: null }));
-        console.log("Heartbeat sent.");
+        logDebug("Heartbeat sent.");
       }, heartbeatIntervalMs);
 
-      // Set initial activity after connecting
       setTimeout(setRichPresence, 1000);
     }
   });
 
   ws.on("close", (code, reason) => {
-    console.log(`Disconnected. Code: ${code}, Reason: ${reason || "No reason provided."}`);
+    if (code === 1005) return;
+    logError(
+      `Disconnected from discord gateway with: Code: ${code}, Reason: ${
+        reason || "No reason provided"
+      }`
+    );
 
     if (heartbeatInterval) clearInterval(heartbeatInterval);
-    
-    // Attempt to reconnect unless it's a critical error
+
     if (shouldReconnect && ![4004, 4010, 4011].includes(code)) {
-      console.log("Reconnecting in 5 seconds...");
+      logInfo("Reconnecting in 5 seconds...");
       setTimeout(connectUserGateway, 5000);
+    } else if (!shouldReconnect) {
+      return;
     } else {
-      if (!shouldReconnect) return;
-      console.error("Critical error. Manual intervention required.");
+      logError("Critical error. Manual intervention required.");
     }
   });
 
   ws.on("error", (error) => {
-    console.error("WebSocket error:", error);
+    logError("Discord WebSocket error:", error);
   });
 }
 
+function shutdown() {
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send(
+      JSON.stringify({
+        op: 3,
+        d: {
+          activities: [],
+          since: null,
+          status: "offline",
+          afk: false,
+        },
+      })
+    );
+    ws.send(
+      JSON.stringify({
+        op: 3,
+        d: {
+          status: "offline",
+        },
+      })
+    );
+    shouldReconnect = false;
+    logDebug("Activity cleared before shutting down WebSocket...");
+    ws.close();
+  }
+}
 
-bot.on("messageCreate", (message) => {
-  if (message.channel.id !== CHANNEL_ID) return;
+function disconnectEris() {
+  try {
+    if (eris) {
+      logInfo("Clearing activity before disconnecting Eris...");
+      eris.editStatus("offline", []);
+    }
+    logInfo("Disconnecting Eris...");
+    eris.disconnect();
+    isActivityRunning = false;
+    logInfo("Successfully destroyed Eris.");
+    delete eris;
+  } catch (e) {
+    logError("Failed to disconnect Eris due to unknown exception: " + e);
+    isActivityRunning = false;
+  }
+}
 
-  if (!message.content) return;
+function launchEris() {
+  try {
+    logInfo("Launching Eris...");
+    eris = new Eris(variables.userToken);
+    eris.connect();
+    logInfo("Successfully launched Eris.");
+    isActivityRunning = true;
+    eris.on("error", (e) => {
+      logError("Eris runtime error:", e);
+    });
+  } catch (e) {
+    logError("Failed to launch Eris due to unknown exception: " + e);
+    isActivityRunning = false;
+  }
+}
+
+const commands = {
+  start: (message) => {
+    if (isActivityRunning) {
+      message.reply("**Activity is already running. Use `stop` to restart.**");
+      return;
+    }
+
+    if (isActivityDisabled()) {
+      launchEris();
+    } else {
+      connectUserGateway();
+    }
+
+    isActivityRunning = true;
+    message.reply("**Rich Presence started. Use `stop` to stop it.**");
+  },
+  stop: (message) => {
+    if (!isActivityRunning) {
+      message.reply("**Activity is not running. Use `start` first.**");
+      return;
+    }
+
+    if (isActivityDisabled()) {
+      disconnectEris();
+    } else {
+      shutdown();
+    }
+
+    isActivityRunning = false;
+    message.reply("**Rich Presence stopped. Use `start` to restart.**");
+  },
+  restart: async (message) => {
+    if (!isActivityRunning) {
+      message.reply("**Activity is not running. Use `start` first.**");
+      return;
+    }
+
+    commands.stop(message);
+    commands.start(message);
+    message.reply("**Rich Presence restarted.**");
+  },
+  ping: (message) => {
+    const latency = Math.round(bot.ws.ping);
+    message.reply(`**Pong! Latency: ${latency} ms**`);
+  },
+};
+
+bot.on("messageCreate", async (message) => {
+  if (message.channel.id !== variables.channelId) return;
+  if (message.author.bot) return;
 
   const command = message.content.toLowerCase();
-
-  try{
-    if (command === "start") {
-      if (isActivityRunning) {
-        message.reply("**Activity is already running. Please stop and run it again if you want to restart it.**");
-        return;
-      }
-
-      connectUserGateway();
-      isActivityRunning = true;
-      setTimeout(setRichPresence, 2000); // Ensure WebSocket is connected
-      shouldReconnect = true;
-      message.reply("**Rich Presence started. To stop, simply type `stop`, to restart, simply type `restart`.**");
-    } else if (command === "stop") {
-      if (!isActivityRunning) {
-        message.reply("**Activity is not running. Use `start` first**");
-        return;
-      }
-
-      shouldReconnect = false;
-      if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.close();
-      }
-      isActivityRunning = false;
-      message.reply("**Rich Presence stopped. You can restart it again using `start`.***");
-    } else if (["alive", "ping"].includes(command.toLowerCase())) { 
-      const latency = Math.round(bot.ws.ping)
-      message.reply(`**Pong! Latency: ${latency} ms**`);
-    } else if (command === "restart") {
-      if (!isActivityRunning) {
-        message.reply("**Activity is not running. Use `start` first.**");
-        return;
-      }
-      
-      shouldReconnect = true;
-      if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.close();
-      }
-      connectUserGateway();
-      setTimeout(setRichPresence, 2000);
-      message.reply("**Rich Presence restarted. To stop, simply use `stop`.**");
+  if (commands[command]) {
+    try {
+      await commands[command](message);
+    } catch (error) {
+      logError("Command execution error:", error);
     }
-  }
-  catch (e) { 
-    console.error("Rich presence backend exception: ", e)
+  } else {
+    message.reply("**Unknown command.**");
   }
 });
 
-bot
-  .login(BOT_TOKEN)
-  .then(() => {
-    console.log("Bot is online and ready to monitor commands.");
-  })
-  .catch((e) => {
-    console.error("An error occurred while starting the bot: ", e);
-  });
+bot.once("ready", () => {
+  bot.user.setPresence({ status: "invisible" });
+  logInfo("Bot is online and set to invisible.");
+});
 
-bot.on("error", (e) => {
-  console.error("An error occurred during runtime of the bot: ", e); 
+bot.on("error", (error) => {
+  logError("Bot runtime error:", error);
+});
+
+(async () => {
+  try {
+    await bot.login(variables.botToken);
+    logInfo("Bot logged in successfully.");
+  } catch (error) {
+    logError("Failed to log in for the bot account:", error);
+    process.exit(1);
+  }
+})();
+
+validateEnvVariables();
+
+process.on("SIGINT", () => {
+  logInfo("Shutting down...");
+  if (ws) shutdown();
+  if (eris) disconnectEris();
+  bot.destroy();
+  process.exit(0);
 });
