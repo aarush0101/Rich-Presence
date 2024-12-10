c049ceee042ec77c371f1d86a85ff4d07c79c9b4
aarush0101 <141508418+aarush0101@users.noreply.github.com>
2024-12-10 22:56:24 +0530

    chore: fix all issues

---
+++ src/utilities.js
@@ -0,0 +1,61 @@
+function logInfo(message) {
+  console.log(`[INFO] ${message}`);
+}
+
+function logError(message, error = null) {
+  console.error(`[ERROR] ${message}`, error || "");
+}
+
+function logDebug(message, debug = null) {
+  console.debug(`[DEBUG] ${message}`, debug || "");
+}
+
+function validateEnvVariables() {
+  const requiredVariables = ["BOT_TOKEN", "USER_TOKEN", "CHANNEL_ID", "START"];
+  requiredVariables.forEach((variable) => {
+    if (!process.env[variable]) {
+      logError(`Missing required environment variable: ${variable}`);
+      process.exit(1);
+    }
+  });
+
+  if (isNaN(Number(process.env.TYPE))) {
+    logError("Invalid TYPE in .env file. Must be a number.");
+    process.exit(1);
+  }
+}
+
+function isStartEnabled() {
+  const startStates = ["true", "y", "continue", "y"];
+  return startStates.includes(process.env.START?.toLowerCase() || "");
+}
+
+function isActivityDisabled() {
+  const noActivityStates = ["true", "yes", "continue", "y"];
+  return noActivityStates.includes(
+    process.env.NO_ACTIVITY?.toLowerCase() || ""
+  );
+}
+
+function isTrue(variable) {
+  if (!variable) return false;
+  const states = ["true", "yes", "continue", "y"];
+  return states.includes(process.env[variable]?.toLowerCase() || "");
+}
+
+function isFalse(variable) {
+  if (!variable) return false;
+  const states = ["false", "no", "back", "n"];
+  return states.includes(process.env[variable]?.toLowerCase() || "");
+}
+
+module.exports = {
+  logInfo,
+  logError,
+  validateEnvVariables,
+  isStartEnabled,
+  isActivityDisabled,
+  isFalse,
+  isTrue,
+  logDebug,
+};
